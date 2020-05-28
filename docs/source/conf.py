f9634a009a2586a33c148814fea28bd659e467c8
ssantichaivekin <jsantichaivekin@g.hmc.edu>
2020-05-28 15:25:19 -0700

    Fix read the docs not compiling due to master_doc bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,9 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
